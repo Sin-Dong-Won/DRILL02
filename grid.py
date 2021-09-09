405f12bfd2bacb9a1b1875a12b7818af56c4dfc2
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-09-09 19:21:02 +0900

    I make real grid 500, 100
    I use 'for'

--- grid.py
+++ grid.py
@@ -1,5 +1,23 @@
 import turtle
 turtle.shape('turtle')
-for grid in  range(6):
+turtle.left(90)
+a = 90
+
+for grid in range(5): 
+ turtle.forward(500) 
+ turtle.right(a)
  turtle.forward(100)
- turtle.left(60)
+ turtle.right(a)
+ a = a * -1
+
+turtle.forward(500)
+turtle.right(90)
+turtle.forward(500)
+turtle.right(90)
+
+for grid in range(5): 
+ turtle.forward(100)
+ turtle.left(a)
+ turtle.forward(500)
+ turtle.right(a)
+ a = a * -1
