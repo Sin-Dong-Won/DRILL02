22f7880bead0a1728d022d79e047144061b7bdc5
Sin-Dong-Won <56111933+Sin-Dong-Won@users.noreply.github.com>
2021-09-14 18:42:36 +0900

    Today_Study

---
+++ Draw_circle2.py
@@ -0,0 +1,23 @@
+import turtle
+
+def draw_circle(x,y,r):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.circle(r)
+    turtle.penup()
+    turtle.goto(x,y + (r))    
+    turtle.write(str((x,y)))
+
+
+
+
+turtle.shape('turtle')
+draw_circle(0,0,50)
+draw_circle(200,200,100)
+draw_circle(100,-100,50)
+
+
+    
+
+
